696d5427f3c41082128f7361dfaa6766944ec4f8
takeda <wind.ware1203@gmail.com>
2023-05-16 17:55:38 +0800

    scan 0516

--- shells/test.sh
+++ shells/test.sh
@@ -1,7 +1,11 @@
-date=$(date +"%Y/%m/%d")
+date=$(date +"%Y_%m_%d")
+fileName="./log/lynisLog_${date}.log"
+
 read -p "enter your names:" name
 
 echo ".........hi ${name}............"
-sudo lynis audit system --logfile ./log/lynisLog_${date}.log --auditor ${name}
 
-#sudo chmod +rx ./lynisLog_${date}.log
+sudo lynis audit system --logfile ${fileName} --auditor ${name}
+
+sudo chmod +rwx ${fileName}
+#sudo echo "Auther:${name} at${date}" >> ${fileName}
